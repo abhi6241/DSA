110a70a8ae338c1da0c7fc8adb257ca88fedf593
V ABHIRAM <abhiramvallem2007@gmail.com>
2026-07-25 13:03:47 +0530

    Time: 8 ms (82.43%) | Memory: 11.9 MB (54.55%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // No cycle possible if list has 0 or 1 node
-        if (head == nullptr || head->next == nullptr) {
-            return false;
-        }
-        
         ListNode* slow = head;
         ListNode* fast = head;
         
