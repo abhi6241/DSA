2bba0829ed6a201d66476e4c21e4503892ee5fbf
V ABHIRAM <abhiramvallem2007@gmail.com>
2026-07-30 22:21:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (81.25%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if(i >= 0) {
+                sum = sum + a[i] - '0';
+                i--;
+            }
+            if(j >= 0) {
+                sum = sum + b[j] - '0';
+                j--;
+            }
+
+            ans += (sum % 2) + '0';
+            // Equivalent:
+            // ans.push_back((sum % 2) + '0');
+            // push_back() appends a single character to the end of the string.
+            carry = sum / 2;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
