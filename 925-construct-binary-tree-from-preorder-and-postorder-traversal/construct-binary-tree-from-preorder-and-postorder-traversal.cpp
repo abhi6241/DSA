463991f5e32515699fe4e21c742f7bf1cbdf175a
V ABHIRAM <abhiramvallem2007@gmail.com>
2026-08-27 23:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (92.95%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -11,57 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int>& preorder, vector<int>& postorder,
-                    int preStart, int preEnd,
-                    int postStart, int postEnd) {
-
-        if (preStart > preEnd)
-            return nullptr;
-
-        // First element of preorder is the root.
-        TreeNode* root = new TreeNode(preorder[preStart]);
-
-        if (preStart == preEnd)
-            return root;
-
-        // The next preorder element is the left subtree's root.
-        int leftRoot = preorder[preStart + 1];
-
-        // Find that node in postorder.
-        int i = postStart;
-        while (postorder[i] != leftRoot)
-            i++;
-
-        // Number of nodes in the left subtree.
-        int leftSize = i - postStart + 1;
-
-        // Build left subtree.
-        root->left = build(
-            preorder, postorder,
-            preStart + 1,
-            preStart + leftSize,
-            postStart,
-            i
-        );
-
-        // Build right subtree.
-        root->right = build(
-            preorder, postorder,
-            preStart + leftSize + 1,
-            preEnd,
-            i + 1,
-            postEnd - 1
-        );
-
-        return root;
-    }
+    int preIndex = 0;
+    int postIndex = 0;
 
     TreeNode* constructFromPrePost(vector<int>& preorder,
                                    vector<int>& postorder) {
-        return build(
-            preorder, postorder,
-            0, preorder.size() - 1,
-            0, postorder.size() - 1
-        );
+
+        // Take the next node from preorder as root.
+        TreeNode* root = new TreeNode(preorder[preIndex++]);
+
+        // If root is not the current postorder node,
+        // there is a left subtree to build.
+        if (root->val != postorder[postIndex]) {
+            root->left = constructFromPrePost(preorder, postorder);
+        }
+
+        // If root is still not the current postorder node,
+        // there is a right subtree to build.
+        if (root->val != postorder[postIndex]) {
+            root->right = constructFromPrePost(preorder, postorder);
+        }
+
+        // Current root is now reached in postorder.
+        postIndex++;
+
+        return root;
     }
 };
