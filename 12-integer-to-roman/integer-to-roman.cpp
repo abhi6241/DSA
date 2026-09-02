4edc7a8b7a9737c7a34cadfecc2225a8f1ea074e
V ABHIRAM <abhiramvallem2007@gmail.com>
2026-09-02 23:48:03 +0530

    Time: 4 ms (49.94%) | Memory: 12.1 MB (49.20%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     string intToRoman(int num) {
+
+        // Roman numeral values in descending order
+        // Includes special cases like 900 = CM, 4 = IV, etc.
         vector<int> values = {
             1000, 900, 500, 400,
             100, 90, 50, 40,
             10, 9, 5, 4, 1
         };
 
+        // Corresponding Roman numeral symbols
         vector<string> symbols = {
             "M", "CM", "D", "CD",
             "C", "XC", "L", "XL",
@@ -15,9 +19,16 @@ public:
 
         string ans = "";
 
+        // Check each Roman numeral value from largest to smallest
         for (int i = 0; i < values.size(); i++) {
+
+            // Use the current value as many times as possible
             while (num >= values[i]) {
+
+                // Add the corresponding Roman symbol
                 ans += symbols[i];
+
+                // Reduce num by that value
                 num -= values[i];
             }
         }
