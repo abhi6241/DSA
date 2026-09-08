07a43ddbf62774a3b3bdcae783384b9e843a0c37
V ABHIRAM <abhiramvallem2007@gmail.com>
2026-09-08 21:15:48 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (53.81%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int size = nums.size();
-        int maxReach = 0;
-        int end = 0;
         int count = 0;
+        int end = 0;
+        int maxReach = 0;
 
-        // We don't need to jump if there is only one element
-        if(size == 1)
-            return 0;
+        for(int i = 0; i < nums.size() - 1; i++) {
 
-        for(int i = 0; i < size - 1; i++) {
-
-            // Find the farthest position reachable
-            // from the current range
+            // Farthest position reachable from current range
             maxReach = max(maxReach, i + nums[i]);
 
-            // We have reached the end of the current jump range
+            // Reached the end of the current jump
             if(i == end) {
                 count++;
-
-                // Start a new range
                 end = maxReach;
-
-                // We can already reach the last index
-                if(end >= size - 1)
-                    break;
             }
         }
 
